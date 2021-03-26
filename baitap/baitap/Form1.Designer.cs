5507e31be61645420133ccc0ed7b6bc7d8681424
Hồ Công Hậu <conghau1232@live.com>
2021-03-26 09:39:22 +0700

    Button LOGIN

--- baitap/baitap/Form1.Designer.cs
+++ baitap/baitap/Form1.Designer.cs
@@ -29,63 +29,71 @@ namespace baitap
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.txtusername = new System.Windows.Forms.TextBox();
-            this.lblusername = new System.Windows.Forms.Label();
-            this.lblpassword = new System.Windows.Forms.Label();
-            this.txtpassword = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // txtusername
-            // 
-            this.txtusername.Location = new System.Drawing.Point(348, 87);
-            this.txtusername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtusername.Name = "txtusername";
-            this.txtusername.Size = new System.Drawing.Size(110, 23);
-            this.txtusername.TabIndex = 0;
-            // 
-            // lblusername
-            // 
-            this.lblusername.AutoSize = true;
-            this.lblusername.Location = new System.Drawing.Point(231, 87);
-            this.lblusername.Name = "lblusername";
-            this.lblusername.Size = new System.Drawing.Size(89, 15);
-            this.lblusername.TabIndex = 1;
-            this.lblusername.Text = "Ten Dang Nhap";
-            this.lblusername.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // lblpassword
-            // 
-            this.lblpassword.AutoSize = true;
-            this.lblpassword.Location = new System.Drawing.Point(242, 169);
-            this.lblpassword.Name = "lblpassword";
-            this.lblpassword.Size = new System.Drawing.Size(58, 15);
-            this.lblpassword.TabIndex = 3;
-            this.lblpassword.Text = "Mật Khẩu";
-            this.lblpassword.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // txtpassword
-            // 
-            this.txtpassword.Location = new System.Drawing.Point(348, 155);
-            this.txtpassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtpassword.Name = "txtpassword";
-            this.txtpassword.Size = new System.Drawing.Size(110, 23);
-            this.txtpassword.TabIndex = 2;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 338);
-            this.Controls.Add(this.lblpassword);
-            this.Controls.Add(this.txtpassword);
-            this.Controls.Add(this.lblusername);
-            this.Controls.Add(this.txtusername);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.txtusername = new System.Windows.Forms.TextBox();
+			this.lblusername = new System.Windows.Forms.Label();
+			this.lblpassword = new System.Windows.Forms.Label();
+			this.txtpassword = new System.Windows.Forms.TextBox();
+			this.btnLogin = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtusername
+			// 
+			this.txtusername.Location = new System.Drawing.Point(398, 116);
+			this.txtusername.Name = "txtusername";
+			this.txtusername.Size = new System.Drawing.Size(125, 27);
+			this.txtusername.TabIndex = 0;
+			// 
+			// lblusername
+			// 
+			this.lblusername.AutoSize = true;
+			this.lblusername.Location = new System.Drawing.Point(264, 116);
+			this.lblusername.Name = "lblusername";
+			this.lblusername.Size = new System.Drawing.Size(112, 20);
+			this.lblusername.TabIndex = 1;
+			this.lblusername.Text = "Ten Dang Nhap";
+			this.lblusername.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// lblpassword
+			// 
+			this.lblpassword.AutoSize = true;
+			this.lblpassword.Location = new System.Drawing.Point(277, 225);
+			this.lblpassword.Name = "lblpassword";
+			this.lblpassword.Size = new System.Drawing.Size(72, 20);
+			this.lblpassword.TabIndex = 3;
+			this.lblpassword.Text = "Mật Khẩu";
+			this.lblpassword.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// txtpassword
+			// 
+			this.txtpassword.Location = new System.Drawing.Point(398, 207);
+			this.txtpassword.Name = "txtpassword";
+			this.txtpassword.Size = new System.Drawing.Size(125, 27);
+			this.txtpassword.TabIndex = 2;
+			// 
+			// btnLogin
+			// 
+			this.btnLogin.Location = new System.Drawing.Point(398, 266);
+			this.btnLogin.Name = "btnLogin";
+			this.btnLogin.Size = new System.Drawing.Size(94, 29);
+			this.btnLogin.TabIndex = 4;
+			this.btnLogin.Text = "Login";
+			this.btnLogin.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 451);
+			this.Controls.Add(this.btnLogin);
+			this.Controls.Add(this.lblpassword);
+			this.Controls.Add(this.txtpassword);
+			this.Controls.Add(this.lblusername);
+			this.Controls.Add(this.txtusername);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -95,6 +103,7 @@ namespace baitap
 		private System.Windows.Forms.Label lblusername;
         private System.Windows.Forms.Label lblpassword;
         private System.Windows.Forms.TextBox txtpassword;
-    }
+		private System.Windows.Forms.Button btnLogin;
+	}
 }
 
